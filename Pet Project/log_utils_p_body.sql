d40350fa48aa79c6824a6abb70579b1b775d38ff
Nataliia <yakymyshyn25@gmail.com>
2026-09-17 20:01:15 +0300

    Add call to to_log procedure to procedures of log_utils package, add sql for log table sequence and to_log procedure creation. Complete Task PS-16

--- Pet Project/log_utils_p_body.sql
+++ Pet Project/log_utils_p_body.sql
@@ -3,7 +3,7 @@ CREATE OR REPLACE PACKAGE BODY log_utils AS
 
   PROCEDURE log_start(p_proc_name IN VARCHAR2,
                       p_text IN VARCHAR2 DEFAULT NULL) IS
-  v_text VARCHAR2(300);
+  v_text VARCHAR2(2000);
 
   BEGIN
     IF p_text IS NULL THEN
@@ -11,6 +11,7 @@ CREATE OR REPLACE PACKAGE BODY log_utils AS
         ELSE
         v_text := p_text;    
     END IF;
+    to_log(p_appl_proc => p_proc_name, p_message => v_text);
 
   END log_start; 
 
@@ -19,7 +20,7 @@ CREATE OR REPLACE PACKAGE BODY log_utils AS
 
   PROCEDURE log_finish(p_proc_name IN VARCHAR2,
                       p_text IN VARCHAR2 DEFAULT NULL) IS
-  v_text VARCHAR2(300);
+  v_text VARCHAR2(2000);
 
   BEGIN
     IF p_text IS NULL THEN
@@ -27,6 +28,7 @@ CREATE OR REPLACE PACKAGE BODY log_utils AS
         ELSE
         v_text := p_text;    
     END IF;
+    to_log(p_appl_proc => p_proc_name, p_message => v_text);
 
   END log_finish; 
 
@@ -35,14 +37,16 @@ CREATE OR REPLACE PACKAGE BODY log_utils AS
   PROCEDURE log_error(p_proc_name IN VARCHAR2,
                       p_sqlerrm IN VARCHAR2,
                       p_text IN VARCHAR2 DEFAULT NULL) IS
-  v_text VARCHAR2(300);
+  v_text VARCHAR2(2000);
 
-  BEGIN
+  BEGIN 
+    p_sqlerrm := REPLACE(p_sqlerrm, CHR(10), SQLERRM);
     IF p_text IS NULL THEN
         v_text := 'Error in procedure: ' || p_proc_name || ' - Error: ' || p_sqlerrm;
         ELSE
         v_text := p_text;    
     END IF;
+    to_log(p_appl_proc => p_proc_name, p_message => v_text);
 
   END log_error;
 
